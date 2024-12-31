bf58b7d8fbe9079fd3aed8028025432e7f8034c1
Akanksha Patil <84930402+akankshapatil2015@users.noreply.github.com>
2024-12-31 23:59:49 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ----------Typing Animation-----------*/
+var typed = new Typed('.typing', {
+  strings: ['','Frontend Developer', 'Software Developer', 'Web Developer'],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+});
